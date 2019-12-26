238f04a48dd5ccafac05e4fcafd955ed3d147958
Israel Larrondo <arsistyle@gmail.com>
2019-12-26 14:27:11 -0300

    Cambiando de usuario

--- api/index.js
+++ api/index.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const meals = require('./routes/meals')
-const orders = require('./routes/orders')
-const auth = require('./routes/auth')
+const meals = require('./routes/meals');
+const orders = require('./routes/orders');
+const auth = require('./routes/auth');
 const app = express();
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.use('/api/meals', meals)
-app.use('/api/orders', orders)
-app.use('/api/auth', auth)
+app.use('/api/meals', meals);
+app.use('/api/orders', orders);
+app.use('/api/auth', auth);
 
 module.exports = app;
